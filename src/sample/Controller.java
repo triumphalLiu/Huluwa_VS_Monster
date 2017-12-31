6b2605c2bc1bc606187390a0ad9ee039c427924f
liu <liu@triumphal.cn>
2017-12-31 13:55:37 +0800

    修改 “关于游戏”选项，跳转到网站
    下一步 实现移动和战斗场面
    每一步结果都保存到文件（8*12个位置用字符表示）

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -136,24 +136,13 @@ public class Controller {
 
     @FXML
     void clickAboutButton(MouseEvent event){
-        String email_info = "liu@triumphal.cn";
-        String web_info = "https://github.com/triumphalLiu/Huluwa_VS_Monster";
-        //添加一个包含超链接的对话框
-        LinkButton email = new LinkButton("邮箱：", email_info);
-        LinkButton website = new LinkButton("源码：", web_info);
-        JLabel label = new JLabel("信息已复制到剪贴板:)");
-        JPanel jPanel = new JPanel();
-        jPanel.setLayout(new GridLayout(3,1));
-        jPanel.add(email);
-        jPanel.add(website);
-        jPanel.add(label);
-        //复制内容到剪贴板
-        Clipboard clip = Toolkit.getDefaultToolkit().getSystemClipboard();
-        Transferable tText = new StringSelection(email_info + ", " + web_info);
-        clip.setContents(tText, null);
-        //显示对话框
-        JOptionPane jOptionPane = new JOptionPane();
-        jOptionPane.showMessageDialog(null, jPanel, "关于 葫芦娃V1.0", JOptionPane.INFORMATION_MESSAGE);
+        String cmd = "rundll32 url.dll,FileProtocolHandler https://github.com/triumphalLiu/Huluwa_VS_Monster";
+        Runtime rt = Runtime.getRuntime();
+        try {
+            rt.exec(cmd);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
     @FXML
@@ -211,75 +200,5 @@ class HLWFileFilter extends FileFilter {
     public boolean accept(File file) {
         String name = file.getName();
         return file.isDirectory() || name.toLowerCase().endsWith(".hlw");  // 仅显示.hlw文件
-    }
-}
-
-class LinkButton extends JLabel {
-    private String text;
-    private String disc;
-    private Cursor handCursor = new Cursor(Cursor.HAND_CURSOR);  //建立一个光标为手指类型的鼠标
-
-    public LinkButton(String name, String web) {
-        text = web;
-        disc = name;
-        setText();
-        addMouseListener(new MouseAdapter() {
-            public void mouseClicked(MouseEvent e) {
-                Runtime rt = Runtime.getRuntime();
-                try {
-                    String cmd = "rundll32 url.dll,FileProtocolHandler " + text;
-                    rt.exec(cmd);
-                } catch (IOException e1) {
-                    e1.printStackTrace();
-                }
-                setClickedText();
-            }
-
-            //鼠标移入标签时,设置文本样式事件
-            public void mouseEntered(MouseEvent e) {
-                setHandCursor();
-                setMoveInText();
-            }
-
-            //鼠标移出标签时,设置文本样式事件
-            public void mouseExited(MouseEvent e) {
-                setDefaultCursor();
-                setMoveOutText();
-            }
-        });
-    }
-
-    //设置初始样式
-    private void setText() {
-        String content = "<html><font color=blue>" + this.disc + this.text + "</font></html>";
-        super.setText(content);
-    }
-
-    //设置鼠标单击样式
-    private void setClickedText() {
-        String content = "<html><font color=blue>" + this.disc + this.text + "</font></html>";
-        super.setText(content);
-    }
-
-    //设置鼠标移入样式
-    private void setMoveInText(){
-        String content = "<html><font color=blue>" + this.disc + this.text + "</font></html>";
-        super.setText(content);
-    }
-
-    //设置鼠标移出样式
-    private void setMoveOutText(){
-        String content = "<html><font color=blue>" + this.disc + this.text + "</font></html>";
-        super.setText(content);
-    }
-
-    //设置光标为手指
-    private void setHandCursor() {
-        this.setCursor(handCursor);
-    }
-
-    //设置光标为默认
-    private void setDefaultCursor() {
-        this.setCursor(null);
     }
 }
